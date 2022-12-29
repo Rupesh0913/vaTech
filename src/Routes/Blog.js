9e6440168d2607da3920e1101da7d91cd88983b2
sharduli8090 <shardulipandey8090@gmail.com>
2022-12-29 21:09:36 +0530

    Created form comp and heading comp in blog page

--- src/Routes/Blog.js
+++ src/Routes/Blog.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Form from '../Pages/AboutPage/Form';
+import img from '../Assets/Images/blog.png';
+import '../Assets/Styles/Blog.css';
 
 const Blog = () =>{
     return(
         <>
-        Blog Page Here!
+
+        <img src={img} className="blog-image" alt="Blog Image"/>
         <Form/>
         </>
     );
 }
-export default Blog;
+export default Blog; 
